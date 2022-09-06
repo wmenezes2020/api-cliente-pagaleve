84ca2da66e39d90ec5675e42d4547107ea6a557d
Wesley Menezes <w.menezes@winholding.app>
2022-09-06 01:09:35 -0300

    Build code prject

--- dist/lib/cdk-stack.d.ts
+++
@@ -1,4 +0,0 @@
-import { App, Stack, StackProps } from '@aws-cdk/core';
-export declare class CdkStack extends Stack {
-    constructor(scope: App, id: string, props: StackProps);
-}
--- dist/lib/permissions-boundary-aspect.d.ts
+++
@@ -1,6 +0,0 @@
-import { IAspect, IConstruct } from '@aws-cdk/core';
-export declare class PermissionsBoundaryAspect implements IAspect {
-    private readonly arn;
-    constructor(permissionBoundaryArn: string);
-    visit(node: IConstruct): void;
-}
